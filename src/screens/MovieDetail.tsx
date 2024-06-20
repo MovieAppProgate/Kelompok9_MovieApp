b169421498c2f6a5b24c05d357df104f38e99189
aenulfahir <aenulfahir03@gmail.com>
2024-06-20 10:10:27 +0800

    Integration_API

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStack } from '../navigation/HomeStackNavigation';
+import React from 'react'
+import { View, Text, Button } from 'react-native'
 
-type Props = NativeStackScreenProps<RootStack, 'MovieDetail'>;
+const MovieDetail = ({ navigation }: any): any => {
+  const fetchData = (): void => {
+    // Gantilah dengan akses token Anda
+    const ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMGJjOTk0Y2NiYmM2OWNjYTQ4NGU1MWE0NDQxZGFmNiIsInN1YiI6IjY2NzMwN2U3ODdmYmMzNmY1ZTU1YTcwMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7QjzR37v0dWrz2axuUCA_ZBLVb69toFZLQtWbncIU2Y'
 
-export default function MovieDetail({ navigation }: Props): JSX.Element {
+    const url =
+      'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+    }
+
+    fetch(url, options)
+      .then(async (response) => await response.json())
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
   return (
-    <View>
-      <Text>Movie Detail</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Movie Detail Page</Text>
       <Button
-        title="Go back to Home"
-        onPress={() => navigation.navigate('Home')}
+        title="Fetch Data"
+        onPress={() => {
+          fetchData()
+        }}
       />
     </View>
-  );
+  )
 }
+
+export default MovieDetail
